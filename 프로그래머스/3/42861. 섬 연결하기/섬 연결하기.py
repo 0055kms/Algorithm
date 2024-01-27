a10aac7767279a0b4bf14a152bfe43a568d008a6
Kang Myungseok <157768680+0055kms@users.noreply.github.com>
2024-01-27 09:33:18 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -0,0 +1,20 @@
+def solution(n, costs):
+    parent = [x for x in range(n)]
+    def find(a):
+        if a != parent[a]: parent[a] = find(parent[a])
+        return parent[a]
+    def union(a,b):
+        a,b=find(a),find(b)
+        if a != b: parent[b] = a
+    
+    costs.sort(key = lambda x: x[2], reverse = True)
+    ans = 0
+    cnt = 0
+    while cnt < n-1:
+        s,e,w = costs.pop()
+        if find(s) != find(e):
+            ans += w
+            cnt += 1
+            union(s,e)
+    
+    return ans
