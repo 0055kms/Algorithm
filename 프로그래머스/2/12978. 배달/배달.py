d9a13e7165e1d70418887ca29bd900e54b79ddeb
Myungseok Kang <157768680+0055kms@users.noreply.github.com>
2024-02-01 14:30:46 +0900

    [level 2] Title: 배달, Time: 59.15 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,18 +1,27 @@
+from collections import deque
 def solution(N, road, K):
-    ans = 0
-    inf = float("inf")
-    D = [[inf] * (N+1) for _ in range(N+1)]
-    D[1][1] = 0
+    answer = 0
+    Visited = [False] * (N+1)
+    D = [0] * (N+1)
+    Elist = [[] for _ in range(N+1)]
     for s,e,w in road:
-        D[s][e] = min(D[s][e],w)
-        D[e][s] = min(D[e][s],w)
+        Elist[s].append((e,w))
+        Elist[e].append((s,w))
+    for i in range(len(Elist)):
+        Elist[i].sort(key = lambda x: x[1])
         
-    for k in range(N+1):
-        for s in range(N+1):
-            for e in range(N+1):
-                D[s][e] = min(D[s][e],D[s][k] + D[k][e])
-    for i in D[1][1:]:
-        if i <= K: ans +=1
-    return ans
-        
+    def bfs(n):
+        q = deque([n])
+        Visited[n] = True
+        while q:
+            now = q.popleft()
+            for next,d in Elist[now]:
+                if not Visited[next]:
+                    Visited[next] = True
+                    D[next] = D[now] + d
+                    q.append(next)
+    bfs(1)
+    for i in D[1:]:
+        if i <= K: answer += 1
+    return Elist
     
