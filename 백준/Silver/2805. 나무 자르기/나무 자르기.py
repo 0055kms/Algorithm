a10f081453eeb25f0553c45790693aed9408dc1a
Myungseok Kang <dooly6276@gmail.com>
2024-08-07 13:14:24 +0900

    [Silver II] Title: 나무 자르기, Time: 4696 ms, Memory: 152324 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,17 @@
+import sys
+from math import ceil
+input = sys.stdin.readline
+N,need = map(int,input().split())
+wood = [x for x in map(int,input().split())]
+s,e = 0,max(wood)
+while s<e:
+    m = (s+e)//2
+    get = 0
+    for w in wood:
+        if w>=m: get += (w-m)
+    if get>= need: s = m +1
+    else: e = m
+print(s-1)
+"""
+
+"""
