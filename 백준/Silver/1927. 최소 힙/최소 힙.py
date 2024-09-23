fc7c0981e1910521db83fcdd675cfd55e200baea
Myungseok Kang <dooly6276@gmail.com>
2024-09-23 22:42:46 +0900

    [Silver II] Title: 최소 힙, Time: 104 ms, Memory: 37044 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -1,13 +1,9 @@
 import sys
+from heapq import *
 input = sys.stdin.readline
-from queue import PriorityQueue
+hq = []
 N = int(input())
-rlist = []
-q = PriorityQueue()
 for _ in range(N):
-    rlist.append(int(input()))
-for i in rlist:
-    if i == 0:
-        if q.qsize() == 0: print(0)
-        else: print(q.get())
-    else: q.put(i)
+    x = int(input())
+    if x == 0: print(0 if not hq else heappop(hq))
+    else: heappush(hq,x)
