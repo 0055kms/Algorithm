9b2a01c919ac4c4f2f8f91b8f3c98124901e997b
Myungseok Kang <dooly6276@gmail.com>
2024-11-30 12:39:23 +0900

    [level 3] Title: 입국심사, Time: 564.16 ms, Memory: 14.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,14 +1,18 @@
 def solution(n, times):
-    times.sort()
-    answer = 0
-    s,e = 0,times[-1] * n
+    s,e = min(times),n*max(times)
+    
+    def check(hour):
+        cnt = 0
+        for t in times:
+            cnt += (hour//t)
+        if cnt >= n: return True
+        else: return False
+    
     while s<=e:
         m = (s+e)//2
-        tmp_ans = 0
-        for time in times: tmp_ans += (m//time)
-        if tmp_ans >= n: #m분의 시간으로 n명이상 처리가 가능
+        if check(m): #m시간 안에 심사 가능함?
             e = m-1
-        else: #불가능
+        else:
             s = m+1
-    
-    return e+1
+            
+    return s
